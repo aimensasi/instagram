1a2fde4e235c1451b220f37af8d111148fde6c6c
aimensasi <ali.sasi@ymail.com>
2016-12-26 21:30:10 +0800

    added seed file

--- Gemfile
+++ Gemfile
@@ -41,10 +41,11 @@ gem "mini_magick"
 #search pg
 gem 'pg_search'
 
+gem 'faker', '~> 1.6', '>= 1.6.6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
-  gem 'faker', '~> 1.6', '>= 1.6.6'
+  gem 'byebug', platform: :mri  
   gem 'rspec-rails'
   gem 'rails-controller-testing'
   gem 'guard-rspec'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,38 @@
-# user = User.new({
-# 	:name => Faker::Name.name,
-# 	:username => Faker::Internet.user_name,
-# 	:email => Faker::Internet.email,
-# 	:bio => Faker::Lorem.paragraph(3),
-# 	:phone_number => Faker::PhoneNumber.phone_number,
-# 	:gender => ['male', 'female'].sample,
-# 	:password => "123123",
-# 	:avatar => 
-# })
+Dir.glob('app/assets/images/seed_images/avatars/*') do |photo|
+	user = User.new({
+		:name => Faker::Name.name,
+		:username => Faker::Internet.user_name,
+		:email => Faker::Internet.email,
+		:bio => Faker::Lorem.paragraph(3),
+		:phone_number => Faker::PhoneNumber.phone_number,
+		:gender => ['male', 'female'].sample,
+		:password => "123123",
+		:avatar => File.open(photo)
+	})
 
-# if user.save
-# 	puts "Created User Number #{n}"
-# else
-# 	puts "ERROR:::=>> #{user.errors.full_messages}"
-# end
+	if user.save
+		puts "Created User Number #{user.id}"
+	else
+		puts "ERROR:::=>> #{user.errors.full_messages}"
+	end
 
-# 10.times do |n|
-# 	user = User.new({
-# 		:name => Faker::Name.name,
-# 		:username => Faker::Internet.user_name,
-# 		:email => Faker::Internet.email,
-# 		:bio => Faker::Lorem.paragraph(3),
-# 		:phone_number => Faker::PhoneNumber.phone_number,
-# 		:gender => ['male', 'female'].sample,
-# 		:password => "123123"
-# 	})
+end
 
-# 	if user.save
-# 		puts "Created User Number #{n}"
-# 	else
-# 		puts "ERROR:::=>> #{user.errors.full_messages}"
-# 	end
-# end
 
-# users_ids = User.ids
+users_ids = User.ids
 
-# Dir.glob('/Users/AIMEN.S.A/Sites/PHOTO/*.jpg') do |photo|
+Dir.glob('app/assets/images/seed_images/posts/*') do |photo|
 	
-#  	post = Post.new({
-#  		:media => File.open(photo),
-#  		:caption => Faker::Lorem.paragraph(2),
-#  		:user_id => users_ids.sample
-#  	})
+ 	post = Post.new({
+ 		:media => File.open(photo),
+ 		:caption => Faker::Lorem.paragraph(2),
+ 		:user_id => users_ids.sample
+ 	})
 
-# 	if post.save
-# 	 	puts "Created Post "
-# 	else
-# 	 	puts "ERROR:::=>> #{post.errors.full_messages}"
-# 	end	
-# end
+	if post.save
+	 	puts "Created Post #{post.id}"
+	else
+	 	puts "ERROR:::=>> #{post.errors.full_messages}"
+	end	
+end
 
